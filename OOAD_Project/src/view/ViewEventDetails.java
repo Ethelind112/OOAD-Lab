1f67454a0854dbe5b4050d299900c71ebbd5e1d3
Darren Anthony Beltham <darrenanthonybeltham@gmail.com>
2024-12-20 11:02:52 +0700

    Update ViewEventDetails.java

--- OOAD_Project/src/view/ViewEventDetails.java
+++ OOAD_Project/src/view/ViewEventDetails.java
@@ -1,193 +1,104 @@
 package view;
 
-import controller.EventController;
-import controller.GuestController;
-import controller.UserController;
-import controller.VendorController;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TableRow;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
 import model.Event;
 
-public class ViewEventDetails {
-	
-	private Scene eventDetailScene;
-	VBox eventDetailContainer;
-	BorderPane eventDetailPage;
-	Label eventName, eventDescription, eventDate, eventLocation, dateLbl, locationLbl;
-	
-	HBox location, date;
-	
-	MenuBar menubar;
-	Menu invitation, event, updateProfile, users, manageVendor, createEvent;
-	MenuItem iInvitation, iEvent, iUpdateProfile, iUsers, iManageVendor, iCreateEvent;
-	
-	public void initEventDetail() {
-		eventDetailContainer = new VBox();
-		eventDetailPage = new BorderPane();
-		eventDetailScene = new Scene(eventDetailPage, 1000, 700);
-		eventName = new Label();
-		eventDescription = new Label();
-		eventDate = new Label();
-		eventLocation = new Label();
-		dateLbl = new Label("Date: ");
-		locationLbl = new Label("Location: ");
-		location = new HBox();
-		date = new HBox();
-		
-		menubar = new MenuBar();
-		updateProfile = new Menu("Update Profile");
-		iUpdateProfile = new MenuItem("Update Profile");
-	}
-	
-	public void initDetailComponent() {
-		location.getChildren().addAll(locationLbl, eventLocation);
-		date.getChildren().addAll(dateLbl, eventDate);
-		eventDetailContainer.getChildren().addAll(eventName, eventDescription, date, location);
-		eventDetailPage.setTop(menubar);
-		eventDetailPage.setCenter(eventDetailContainer);
-	}
-	
-	public void eventDetailStyling() {
-		eventDetailPage.setStyle("-fx-background-color: white;");
-		eventDetailContainer.setMaxWidth(900);
-		menubar.setPadding(new Insets(10, 10, 10, 10));
-		
-		eventName.setFont(Font.font("Verdana", FontWeight.BOLD, 25));
-		eventDetailContainer.setMargin(eventName, new Insets(50,0,10,0));
-		eventDetailContainer.setMargin(eventDescription, new Insets(0,0,20,0));
-		eventDetailContainer.setAlignment(Pos.TOP_CENTER);
-		location.setAlignment(Pos.TOP_CENTER);
-		date.setAlignment(Pos.TOP_CENTER);
-	}
-	
-	public ViewEventDetails() {
-		initEventDetail();
-		initDetailComponent();
-		eventDetailStyling();
-	}
-	
-	public void setGuestMenu() {
-		invitation = new Menu("Invitations");
-		event = new Menu("Events");
-		
-		iInvitation = new MenuItem("Invitation");
-		iEvent = new MenuItem("Accepted Events");
-		
-		invitation.getItems().addAll(iInvitation);
-		event.getItems().addAll(iEvent);
-		updateProfile.getItems().addAll(iUpdateProfile);
-		
-		menubar.getMenus().addAll(invitation, event, updateProfile);
+import java.util.ArrayList;
 
-	}
-	
-	public void setAdminMenu() {
-		users = new Menu("Users");
-		event = new Menu("Events");
-		
-		iUsers = new MenuItem("Users");
-		iEvent = new MenuItem("Events");
-		
-		users.getItems().addAll(iUsers);
-		event.getItems().addAll(iEvent);
-		updateProfile.getItems().addAll(iUpdateProfile);
-		
-		menubar.getMenus().addAll(event, users, updateProfile);
+import controller.EventOrganizerController;
 
-	}
-	
-	public void setVendorMenu() {
-		invitation = new Menu("Invitations");
-		event = new Menu("Events");
-		manageVendor = new Menu("Manage Vendor");
-		
-		iInvitation = new MenuItem("Invitation");
-		iEvent = new MenuItem("Accepted Events");
-		iManageVendor = new MenuItem("Manage Vendor");
-		
-		invitation.getItems().addAll(iInvitation);
-		event.getItems().addAll(iEvent);
-		manageVendor.getItems().addAll(iManageVendor);
-		updateProfile.getItems().addAll(iUpdateProfile);
-		
-		menubar.getMenus().addAll(invitation, event, manageVendor, updateProfile);
+public class ViewEventDetails extends Application {
 
-	}
-	
-	public void setEventOrganizerMenu() {
-		invitation = new Menu("Invitations");
-		event = new Menu("Events");
-		createEvent = new Menu("Create Event");
-		
-		iInvitation = new MenuItem("Invitation");
-		iEvent = new MenuItem("Accepted Events");
-		iCreateEvent = new MenuItem("Create Event");
-		
-		invitation.getItems().addAll(iInvitation);
-		event.getItems().addAll(iEvent);
-		createEvent.getItems().addAll(iCreateEvent);
-		updateProfile.getItems().addAll(iUpdateProfile);
-		
-		menubar.getMenus().addAll(invitation, event, createEvent, updateProfile);
+    private ViewEvents view;
+	private String email;
+	private EventOrganizerController eventOrganizerController = new EventOrganizerController(view, email);
 
-	}
-	
-	public Scene getScene() {
-		return eventDetailScene;
-	}
-	
-	public void setUserMenu(EventHandler<ActionEvent> handler) {
-		iUsers.setOnAction(handler);
-	}
-	
-	public void setInvitationMenu(EventHandler<ActionEvent> handler) {
-		iInvitation.setOnAction(handler);
-	}
-	
-	public void setManageVendorMenu(EventHandler<ActionEvent> handler) {
-		iManageVendor.setOnAction(handler);
-	}
-	
-	public void setChangeProfileMenu(EventHandler<ActionEvent> handler) {
-		iUpdateProfile.setOnAction(handler);
-	}
-	
-	public void setEventMenu(EventHandler<ActionEvent> handler) {
-		iEvent.setOnAction(handler);
-	}
-	
-	public void setEventName(String name) {
-		eventName.setText(name);
-	}
-	
-	public void setEventDesc(String desc) {
-		eventDescription.setText(desc);
-	}
-	
-	public void setEventDate(String date) {
-		eventDate.setText(date);
-	}
-	
-	public void setEventLoc(String location) {
-		eventLocation.setText(location);
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("View Event Details");
+        VBox root = new VBox(10);
+        root.setPadding(new Insets(10));
+
+        Label eventListLabel = new Label("Organized Events:");
+        ListView<String> eventListView = new ListView<>();
+        ObservableList<String> eventItems = FXCollections.observableArrayList();
+        ArrayList<Event> events = eventOrganizerController.viewOrganizedEvents();
+
+        for (Event event : events) {
+            eventItems.add(event.getEvent_id() + " - " + event.getEvent_name());
+        }
+        
+        eventListView.setItems(eventItems);
+
+        Label eventDetailsLabel = new Label("Event Details:");
+        TextArea eventDetailsArea = new TextArea();
+        eventDetailsArea.setEditable(false);
+
+        eventListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                String selectedEventID = newValue.split(" - ")[0];
+                String details = eventOrganizerController.viewOrganizedTransactionDetails(selectedEventID);
+                eventDetailsArea.setText(details);
+            }
+        });
+        
+        Label editEventLabel = new Label("Edit Event Name:");
+        TextField newNameField = new TextField();
+        Button updateNameButton = new Button("Update Event Name");
+        Label editResultLabel = new Label();
+
+        updateNameButton.setOnAction(e -> {
+            String selectedEvent = eventListView.getSelectionModel().getSelectedItem();
+            if (selectedEvent != null) {
+                String selectedEventID = selectedEvent.split(" - ")[0];
+                String newName = newNameField.getText();
+                String result = eventOrganizerController.editEventName(selectedEventID, newName);
+                editResultLabel.setText(result);
+                eventItems.clear();
+                for (Event event : eventOrganizerController.viewOrganizedEvents()) {
+                    eventItems.add(event.getEvent_id() + " - " + event.getEvent_name());
+                }
+            } else {
+                editResultLabel.setText("Please select an event first.");
+            }
+        });
+
+        Button addVendorButton = new Button("Add Vendor");
+        addVendorButton.setOnAction(e -> {
+            ViewAddVendor viewAddVendor = new ViewAddVendor();
+            try {
+                viewAddVendor.start(new Stage());
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        Button addGuestButton = new Button("Add Guest");
+        addGuestButton.setOnAction(e -> {
+            ViewAddGuest viewAddGuest = new ViewAddGuest();
+            try {
+                viewAddGuest.start(new Stage());
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        root.getChildren().addAll(eventListLabel, eventListView, eventDetailsLabel, eventDetailsArea, editEventLabel, newNameField, updateNameButton, editResultLabel, addVendorButton, addGuestButton);
+
+        Scene scene = new Scene(root, 600, 500);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
